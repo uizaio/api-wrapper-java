4778279507b704a938d57bda46e0cd79fd3831c6
Nguyễn Lương Huy <nguyenluonghuy1996@gmail.com>
2019-02-19 15:50:20 +0700

    Create add storage API with unit test (#15)
    * Create add storage API with unit test
    
    * Fix add storage method name

--- src/main/java/co/uiza/apiwrapper/model/Storage.java
+++ src/main/java/co/uiza/apiwrapper/model/Storage.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
 import co.uiza.apiwrapper.exception.BadRequestException;
 import co.uiza.apiwrapper.exception.UizaException;
 import co.uiza.apiwrapper.net.ApiResource;
@@ -13,6 +14,44 @@ public class Storage extends ApiResource {
 
   private static final String CLASS_DEFAULT_PATH = "media/storage";
 
+  public enum StorageType {
+    @SerializedName("ftp")
+    FTP("ftp"),
+
+    @SerializedName("s3")
+    S3("s3");
+
+    private final String storageType;
+
+    public String getStorageType() {
+      return storageType;
+    }
+
+    private StorageType(String storageType) {
+      this.storageType = storageType;
+    }
+  }
+
+  /**
+   * You can sync your storage (FTP, AWS S3) with UIZA.
+   * After sync, you can select your content easier from your storage to create entity.
+   *
+   * @param storageParams a Map object storing key-value pairs of request parameter
+   */
+  public static JsonObject addStorage(Map<String, Object> storageParams) throws UizaException {
+    JsonElement response =
+        request(RequestMethod.POST, buildRequestURL(CLASS_DEFAULT_PATH), storageParams);
+    String id = getId((JsonObject) checkResponseType(response));
+
+    return retrieveStorage(id);
+  }
+
+  /**
+   * Get information of your added storage (FTP or AWS S3)
+   *
+   * @param id An id of storage to retrieve
+   *
+   */
   public static JsonObject retrieveStorage(String id) throws UizaException {
     if (id == null || id.isEmpty()) {
       throw new BadRequestException(ErrorMessage.BAD_REQUEST_ERROR, "", 400);
